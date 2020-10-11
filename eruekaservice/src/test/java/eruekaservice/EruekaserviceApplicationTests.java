7376f53fac67d0c1a59d45edf9e23a325e474922
anandprakashprabakaran <anandprakashprabakaran@192.168.1.8>
2020-10-11 12:19:08 -0700

    erueka test

---
+++ eruekaservice/src/test/java/eruekaservice/EruekaserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package eruekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EruekaserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
